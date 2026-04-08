5fff6c5629607e296fb49ec76c776d5a86ece98f
Máximo Jofré <maximoalexandro464@gmail.com>
2026-04-08 08:36:03 -0400

    Grid hecha

--- Frontend/src/pages/Horario.tsx
+++ Frontend/src/pages/Horario.tsx
@@ -1,9 +1,34 @@
-export default function Horario() {
+import { Fragment, type ReactElement } from "react"
+import "../App.css"
+import "./Horario.css"
+
+export default function Horario(): ReactElement {
+  const dias = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO"]
+  const bloques = ["A", "B", "C", "C2", "D", "E", "F", "G"]
+
   return (
-    <>
-      <section id="center">
-        
-      </section>
-    </>
-  );
+    <section id="center">
+      <div className="malla">
+        <div className="celda esquina" />
+
+        {dias.map((dia) => (
+          <div key={`header-${dia}`} className="celda encabezado-dia">
+            {dia}
+          </div>
+        ))}
+
+        {bloques.map((bloque) => (
+          <Fragment key={`fila-${bloque}`}>
+            <div className="celda encabezado-bloque">
+              {bloque}
+            </div>
+
+            {dias.map((dia) => (
+              <div key={`celda-${bloque}-${dia}`} className="celda contenido" />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  )
 }
